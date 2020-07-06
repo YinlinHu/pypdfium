7d94cec122eb645276e11e13a53f09db2b7874e9
Yinlin <huyinlin@gmail.com>
2020-07-06 09:13:30 +0200

    add project description for pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 from setuptools import setup
 from pypdfium import __version__
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+    
 setup(
       name='pypdfium',
       version=__version__,
-      description='simple python wrapping of PDFium',
-      keywords='PDFium',
-      url='',
+      description='A simple python wrapper for PDFium',
+      long_description=readme(),
+      long_description_content_type="text/markdown",
+      keywords='Python PDFium',
+      url='https://github.com/YinlinHu/pypdfium',
       author='Yinlin Hu',
       author_email='huyinlin@gmail.com',
       license='MIT License',
